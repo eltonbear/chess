7aeb548c55bf7eeccb2c3f763c6c4f2ae9327bd1
eltonbear <eltonbear@berkeley.edu>
2017-02-13 12:38:27 -0800

    added subclass bishop

---
+++ @Bishop/Bishop.m
@@ -0,0 +1,12 @@
+classdef Bishop < ChessPiece
+    %UNTITLED5 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    methods
+        function symbol = getSymbol(piece)
+            symbol = 'B';
+        end
+    end
+    
+end
+
