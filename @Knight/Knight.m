ebeb8265f7a37e75a7585ad86aa029ddcb7e65cd
eltonbear <eltonbear@berkeley.edu>
2017-02-13 12:34:59 -0800

    added subclass knight

---
+++ @Knight/Knight.m
@@ -0,0 +1,12 @@
+classdef Knight < ChessPiece
+    %UNTITLED3 Summary of this class goes here
+    %   Detailed explanation goes here
+
+    methods
+        function symbol = getSymbol(piece)
+            symbol = 'K';
+        end
+    end
+    
+end
+
