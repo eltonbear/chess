22589ca51c1005b24419fd951276f01e14f1c956
eltonbear <eltonbear@berkeley.edu>
2017-02-13 12:17:57 -0800

    added symbol methond

--- @King/King.m
+++ @King/King.m
@@ -2,6 +2,10 @@ classdef King < ChessPiece
     methods
         function kingPiece = King(location, board, team, chessGame)
             kingPiece = kingPiece@ChessPiece(location, board, team);
-        end        
+        end  
+        
+        function symbol = getSymbol(piece)
+            symbol = 'N';
+        end
     end
 end
