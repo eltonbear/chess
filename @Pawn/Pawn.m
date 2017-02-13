22a2d99a5c97dcecf50fabff3e6daca567ca157c
eltonbear <eltonbear@berkeley.edu>
2017-02-13 12:39:46 -0800

    added subclass pawn

---
+++ @Pawn/Pawn.m
@@ -0,0 +1,12 @@
+classdef Pawn < ChessPiece
+    %PAWN Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    methods
+        function symbol = getSymbol(piece)
+            symbol = 'P';
+        end
+    end
+    
+end
+
