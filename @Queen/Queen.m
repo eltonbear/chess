dd35bafe0e4bc5d07cf2c640e78ea3c7e859c1e3
eltonbear <eltonbear@berkeley.edu>
2017-02-13 12:36:38 -0800

    added subclass queen

---
+++ @Queen/Queen.m
@@ -0,0 +1,12 @@
+classdef Queen < ChessPiece
+    %UNTITLED4 Summary of this class goes here
+    %   Detailed explanation goes here
+
+    methods
+        function symbol = getSymbol(piece)
+            symbol = 'Q';
+        end
+    end
+    
+end
+
