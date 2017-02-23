80cc0974e6d3fe2b3c1c1444cedcc80afcb6a9f8
eltonbear <eltonbear@berkeley.edu>
2017-02-22 17:46:31 -0800

    finished bishop getMoveArray methd

--- @Bishop/Bishop.m
+++ @Bishop/Bishop.m
@@ -10,6 +10,63 @@ classdef Bishop < ChessPiece
         function symbol = getSymbol(piece)
             symbol = 'B';
         end
+        
+        function directions = getMoveArray(piece)
+            row = piece.Position(1);
+            col = piece.Position(2);
+            % Up right
+            directions = {};
+            position = [];
+            r = row + 1;
+            c = col + 1;
+            while r <= 8 && c <= 8
+               position = [position ;[r c]];
+               r = r + 1;
+               c = c + 1;
+            end
+            if isempty(position) == 0
+                directions = [directions {position}];
+            end
+            % Up left
+            position = [];
+            r = row + 1;
+            c = col - 1;
+            while r <= 8 && c >= 1
+               position = [position ;[r c]];
+               r = r + 1;
+               c = c - 1;
+            end
+            if isempty(position) == 0
+                directions = [directions {position}];
+            end
+            
+            % Down right
+            position = [];
+            r = row - 1;
+            c = col + 1;
+            while r >= 1 && c <= 8
+               position = [position ;[r c]];
+               r = r - 1;
+               c = c + 1;
+            end
+            if isempty(position) == 0
+                directions = [directions {position}];
+            end
+            
+            % Down LEFT
+            position = [];
+            r = row - 1;
+            c = col - 1;
+            while r >= 1 && c >= 1
+               position = [position ;[r c]];
+               r = r - 1;
+               c = c - 1;
+            end
+            if isempty(position) == 0
+                directions = [directions {position}];
+            end
+            
+        end
     end
     
 end
