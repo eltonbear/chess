21f5175c3bf52d36950d2265a3b3e81d7084b8e2
eltonbear <eltonbear@berkeley.edu>
2017-02-24 12:05:31 -0800

    added getMoveArray

--- @Pawn/Pawn.m
+++ @Pawn/Pawn.m
@@ -1,14 +1,24 @@
 classdef Pawn < ChessPiece
     %PAWN Summary of this class goes here
     %   Detailed explanation goes here
+    properties
+        Direction
+        Attack1
+        Attack2
+    end
     
     methods
         function pawnPiece = Pawn(location, board, team)
             pawnPiece = pawnPiece@ChessPiece(location, board, team);
+            pawnPiece.initializeProperties(team)
         end  
         
         function symbol = getSymbol(piece)
             symbol = 'P';
+        end
+        
+        function directions = getMoveArray(piece)
+            directions = [];
         end
     end
     
