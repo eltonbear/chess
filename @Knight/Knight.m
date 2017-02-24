d5c82e2df026235f0b6c45cc409b061cf319e43b
eltonbear <eltonbear@berkeley.edu>
2017-02-24 02:43:34 -0800

    added getMoveArray method for Knight

--- @Knight/Knight.m
+++ @Knight/Knight.m
@@ -10,6 +10,32 @@ classdef Knight < ChessPiece
         function symbol = getSymbol(piece)
             symbol = 'N';
         end
+        
+        function directions = getMoveArray(piece)
+            y = piece.Position(2);
+            x = piece.Position(1);
+            directions = {};
+            for row = [y-1 y+1]
+                if row > 0 && row < 9
+                    for col = [x-2 x+2]
+                        if col > 0 && col < 9
+                            directions = [directions [col row]];
+                        end
+                    end
+                end
+            end
+            
+            for row = [y-2 y+2]
+                if row > 0 && row < 9
+                    for col = [x-1 x+1]
+                        if col > 0 && col < 9
+                            directions = [directions [col row]];
+                        end
+                    end
+                end
+            end
+        end
+        
     end
     
 end
