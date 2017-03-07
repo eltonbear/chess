2c3b28cd661f687c29731ee7843d8a335f311256
eltonbear <eltonbear@berkeley.edu>
2017-03-06 18:23:32 -0800

    fixed bugs

--- @LinkedList/add.m
+++ @LinkedList/add.m
@@ -1,23 +1,21 @@
 function add(list,NodeData,index)
              if nargin == 3
-                if index < 1 || index > list.Size
+                if index < 1 || index > list.Size + 1
                     error('Index invaid')
                 end
                 
                 newNode = dlnode(NodeData);
-                % add node to empty list
-                if list.Size == 0   
-                    list.firstNode = newNode;
-                    list.lastNode = newNode;
-                    list.CellArray = {NodeData};
-                end
                 
                 if index == list.Size + 1 % Append
+                    if list.Size == 0 % add node to empty list
+                        list.firstNode = newNode;
+                        list.lastNode = newNode;
+                    end
                     newNode.insertAfter(list.lastNode)
                     list.CellArray = [list.CellArray {NodeData}];
                     list.lastNode = newNode; 
                 else
-                    list.CellArray = [list.CellArray(1:index-1) {NodeData} list.CellArray(index+1:list.Size + 1)];
+                    list.CellArray = [list.CellArray(1:index-1) {NodeData} list.CellArray(index:list.Size)];
                     if index > list.Size/2;
                         targetNode = list.lastNode;
                         for i = list.Size:-1:index
@@ -34,6 +32,9 @@ function add(list,NodeData,index)
                                 newNode.insertBefore(targetNode)
                             end
                             targetNode = targetNode.Next;
+                        end
+                        if index == 1
+                            list.firstNode = newNode;                            
                         end
                     end
                 end
