51c04265227d93f0c95c1b34fcf1753da964cb5b
eltonbear <eltonbear@berkeley.edu>
2017-03-13 15:56:16 -0700

    added all stuffs on the interface

--- @ChessGame/playui.m
+++ @ChessGame/playui.m
@@ -2,50 +2,74 @@ function playui(game)
 %PLAYUI Summary of this function goes here
 %   Detailed explanation goes here
 
-if isempty(game.Playing) || game.Playing == 0
-    if game.Playing == 0
-        disp('Game has ended. Resetting board.');
+    if isempty(game.Playing) || game.Playing == 0
+        if game.Playing == 0
+            disp('Game has ended. Resetting board.');
+        end
+        game.Directions = [];
+        game.Board = ChessBoard();
+        game.Playing = 1;
+        board = game.Board;
+
+
+        % initialize ui
+        if isempty(game.Figure)
+            game.Figure = figure('Name','Chess game');
+        end
+        fWidth = 560;
+        fHeight = 300;
+        fpos = get(game.Figure,'Position');
+        set(game.Figure,'Position',[fpos(1:2), fWidth, fHeight]);
+
+
+        % Fill in the code to assign to the following variables:
+        boardPanel = uipanel(game.Figure, 'Position',[0 0 .5 1]);
+        boardAxes = axes('Parent',boardPanel,'Position',[0.1 0.1 0.8 0.8]);
+        buttonPanel = uipanel(game.Figure, 'Position',[.5 0 .5 1]);
+
+        pieceTextW = 70;
+        pieceTextH = 20;
+        pieceText = uicontrol('Parent', buttonPanel,'Style','text',...
+                              'Position',[fWidth/4-pieceTextW/2 260 pieceTextW pieceTextH],...
+                              'String','Select a piece');               
+        pieceMenuW = 80;
+        pieceMenu = uicontrol('Parent', buttonPanel,'Style','popup',...
+                              'Position',[fWidth/4-pieceMenuW/2 245 pieceMenuW pieceTextH],...
+                              'String',{'King','Queen','Bishop','Rook','Knight','Pawn'}); 
+        positionTextW = 90;
+        positionText = uicontrol('Parent', buttonPanel,'Style','text',...
+                              'Position',[fWidth/4-positionTextW/2 210 positionTextW pieceTextH],...
+                              'String','Enter a position');
+        positionEditW = 70;
+        positionEdit = uicontrol('Parent', buttonPanel, 'Style', 'edit',...
+                                 'Position', [fWidth/4-positionEditW/2 195 positionEditW pieceTextH]);
+        buttonPanelW = 110;
+        buttonPanelH = 70;
+        teamGroup = uibuttongroup('Parent', buttonPanel, 'Visible','on',...
+                                  'Title', 'Select a team', 'Units', 'pixels',...
+                                  'Position', [fWidth/4-buttonPanelW/2 fHeight/2-buttonPanelH/2 buttonPanelW buttonPanelH]);
+        radioButtW = 60;
+        radioButtH = 20;
+        teamButtons{1} = uicontrol('Parent', teamGroup,'Style', 'radiobutton',...
+                                   'String','Team 1',...
+                                   'Position',[buttonPanelW/2-radioButtW/2 buttonPanelH/3*2-radioButtH/2 radioButtW radioButtH]);
+        teamButtons{2} = uicontrol('Parent', teamGroup,'Style', 'radiobutton',...
+                                   'String','Team 2',...
+                                   'Position',[buttonPanelW/2-radioButtW/2 buttonPanelH/3*1-radioButtH/2 radioButtW radioButtH]);
+        buttonW = 70;
+        buttonH = 25;
+        submitButton = uicontrol('Parent', buttonPanel, 'Style', 'pushbutton',...
+                                 'String', 'Submit',...
+                                 'Position', [fWidth/4-buttonW/2 70 buttonW buttonH]);
+
+        playButton = uicontrol('Parent', buttonPanel, 'Style', 'pushbutton',...
+                                'String', 'Play',...
+                                'Position', [fWidth/4-buttonW/2 30 buttonW buttonH]);
+
+        board.display(boardAxes)
+    else
+        game.play();
     end
-    game.Directions = [];
-    game.Board = ChessBoard();
-    game.Playing = 1;
-
-    bd = game.Board;
-
-    % initialize ui
-    if isempty(game.Figure)
-        game.Figure = figure('Name','Chess game');
-    end
-    fpos = get(game.Figure,'Position');
-    set(game.Figure,'Position',[fpos(1:2),560,300]);
-
-
-    % Fill in the code to assign to the following variables:
-    
-    boardPanel = % the left panel
-    boardAxes = % the axes for the board display
-    buttonPanel = % the panel for the buttons
-
-    pieceText = % the text above the piece menu
-    pieceMenu = % the piece menu
-    
-    positionText = % the text above the position box
-    positionEdit = % the position box
-    
-    teamGroup = % the radio button group for team selection
-    teamButtons{1} = % team 1 radio button
-    teamButtons{2} = % team 2 radio button
-
-    submitButton = % the submit button
-
-    playButton = % the play button
-    
-    % put in code here to display the board
-    
-else
-    game.play();
-end
-
 
 
     function submit_callback(hObject,eventdata)
